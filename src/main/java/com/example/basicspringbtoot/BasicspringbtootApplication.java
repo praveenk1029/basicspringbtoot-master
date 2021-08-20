ec84d88f10bdaa567a0621147f4f2a6fed9716c4
praveenk1029 <praveenk1029@gmail.com>
2021-08-19 23:24:24 -0400

    Added new api for showing messages.

---
+++ src/main/java/com/example/basicspringbtoot/BasicspringbtootApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicspringbtoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicspringbtootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicspringbtootApplication.class, args);
+	}
+
+}
